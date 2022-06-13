0dfc2afc59caff5b613a7f785716e400ac52d4df
Abraham Jones <abrahamjones@Abrahams-MacBook-Air.local>
2022-06-13 09:42:12 -0700

    adding react stuff

---
+++ client/src/App.js
@@ -0,0 +1,51 @@
+import React,{useState, useRef} from 'react';
+import './App.css';
+
+function App() {
+  const port = process.env.PORT || 4000
+  const Item = useRef()
+  const [Value, setValue] = useState('null')
+  const [Image, setImage] = useState()
+  const [cPrice, setCprice] = useState('null')
+
+
+  async function sendData(){
+    if(Item.current.value === '') return
+    setValue('loading results...')
+    setCprice('loading results...')
+    setImage(null)
+      fetch(`http://localhost:${port}/post`, {
+          method: 'POST',
+          headers:{'Content-Type':'application/json'},
+          body: JSON.stringify({
+            Item : Item.current.value
+          })
+      })
+      const response = await fetch(`http://localhost:${port}/`)
+      const data = await response.json()
+      setValue(data.average)
+      setImage(data.image)
+      setCprice(data.cheapestPrice)
+  }
+
+  return (
+    <div>
+      <h1>Search for an Item on Amazon and find its average price</h1>
+      <div class='inputs'>
+        <div>
+          <input class="txtBar" type='text' placeholder="search for a item" ref={Item}/>
+          <button class="btn" onClick={sendData}>Search</button>
+        </div>
+      </div>
+      <div class='inputs'>
+        <div>
+        <img class='image' src={`${Image}`} alt='ScrapedImage'/>
+        <p class='result'>Average price of Amazon item: {Value}</p>
+        <p class='result'> Cheapest price for item: {cPrice}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App;
