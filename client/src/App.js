ddac5ad2efbf641dff1ad64e4cc0e1e9977bf4f6
Abraham Jones <abrahamjones@Abrahams-MacBook-Air.local>
2022-06-13 13:39:41 -0700

    added port to fetch url

--- client/src/App.js
+++ client/src/App.js
@@ -13,14 +13,14 @@ function App() {
     setValue('loading results...')
     setCprice('loading results...')
     setImage(null)
-      fetch(`https://find-cheap-prices.herokuapp.com/post`, {
+      fetch(`https://find-cheap-prices.herokuapp.com:${process.env.PORT}/post`, {
           method: 'POST',
           headers:{'Content-Type':'application/json'},
           body: JSON.stringify({
             Item : Item.current.value
           })
       })
-      const response = await fetch(`https://find-cheap-prices.herokuapp.com/`)
+      const response = await fetch(`https://find-cheap-prices.herokuapp.com:${process.env.PORT}/`)
       const data = await response.json()
       setValue(data.average)
       setImage(data.image)
